d40a2ef641502d06fea7c458fe49a4bceca7f30c
Gewuerznud3l <73387417+Gewuerznud3l@users.noreply.github.com>
2021-02-09 17:33:47 +0100

    alles noch kommentiert

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from selenium.webdriver import ActionChains
 import time
 import random
 
-pi = input('pi? [y / n]: ') == 'y'
-noflag = input('use flags? [y / n]: ') == 'n'
-level = int(input('level? '))
-clickable = 'cell size24 hd_closed'
-number = 'cell size24 hd_opened hd_type'
-ticket = ' cell-ticket'
-flag = 'cell size24 hd_closed hd_flag'
-loose = 'top-area-face zoomable hd_top-area-face-lose'
-wonwon = 'top-area-face zoomable hd_top-area-face-win'
+pi = input('pi? [y / n]: ') == 'y'  # nur für meinen pi, wenn ich es darauf laufen lassen will
+noflag = input('use flags? [y / n]: ') == 'n'  # soll der bot mit flaggen spielen?
+level = int(input('level? '))  # levels: 1 - easy, 2 - intermediate, 3 - hard, 4 - custom
+clickable = 'cell size24 hd_closed'  # class eines clickbaren feldes
+number = 'cell size24 hd_opened hd_type'  # class von zahlfeld, nach hd_type kommt gleich die nummer
+ticket = ' cell-ticket'  # zelle, unter der sich ein arenaticket versteckt
+flag = 'cell size24 hd_closed hd_flag'  # flagge
+loose = 'top-area-face zoomable hd_top-area-face-lose'  # das gesicht wenn man verloren hat
+wonwon = 'top-area-face zoomable hd_top-area-face-win'  # das gesicht wenn man gewonnen hat
 wins = 0
 losses = 0
 total = 0
@@ -28,17 +28,17 @@ if level == 3:
     width = 30
     height = 16
 if level == 4:
-    width = int(input('width? '))
+    width = int(input('width? '))  # für custom games die höhe, breite und anzahl an minen
     height = int(input('height? '))
     mines = int(input('mines? '))
 
 print(f'level: {level} ({width}x{height})')
-driver = webdriver.Chrome()
+driver = webdriver.Chrome()  # webdriver initialisieren
 # driver.set_window_size(1280, 720)
-driver.get('https://minesweeper.online/')
+driver.get('https://minesweeper.online/')  # seite laden
 
 # login
-if pi:
+if pi:  # für pi ein anderer weg zur login page
     btn = driver.find_element_by_xpath('//*[@id="header"]/nav/div/div/button')
     btn.click()
     found = False
@@ -64,11 +64,11 @@ while not found:
     found = True
     try:
         input = driver.find_element_by_xpath('//*[@id="sign_in_username"]')
-        input.send_keys('gewuerz.nudel@gmail.com')
+        input.send_keys('gewuerz.nudel@gmail.com')  # account-email
     except:
         found = False
 input = driver.find_element_by_xpath('//*[@id="sign_in_password"]')
-input.send_keys('raspberry')
+input.send_keys('raspberry')  # passwort
 found = False
 while not found:
     found = True
@@ -82,11 +82,11 @@ if level < 4:
     while not found:
         found = True
         try:
-            btn = driver.find_element_by_xpath(f'//*[@id="homepage"]/div[2]/div[{level}]/a/div')
+            btn = driver.find_element_by_xpath(f'//*[@id="homepage"]/div[2]/div[{level}]/a/div')  # zum level laden
             btn.click()
         except:
             found = False
-else:  # custom game
+else:  # nur für custom game
     found = False
     while not found:
         found = True
@@ -121,7 +121,7 @@ else:  # custom game
     btn = driver.find_element_by_xpath('//*[@id="C40_content"]/form/button')
     btn.click()
 
-'''
+''' felder:
 -11 - -19: ignorieren
 -1: leer, clickbar
 0: leer
@@ -132,14 +132,14 @@ else:  # custom game
 '''
 
 
-def find_cells():
+def find_cells():  # findet alle zellen auf der website
     while True:
         check = driver.find_elements_by_xpath('//*[@id="A43"]')
         if len(check) > 0:
             return [[check[0].find_element_by_id(f'cell_{x}_{y}') for x in range(width)] for y in range(height)]
 
 
-def to_num(cells):
+'''def to_num(cells):  # konvertiert zellen in nummern (funktion wird nicht mehr gebraucht, war zu langsam
     field = [[-1 for i in range(width)] for j in range(height)]
     for y in range(height):
         for x in range(width):
@@ -152,9 +152,10 @@ def to_num(cells):
             elif str(cell) == flag:
                 field[y][x] = 9
     return field
+'''
 
 
-def add_num(local_field, posx, posy, searched):
+def add_num(local_field, posx, posy, searched):  # fügt rekursiv alle neuen nummernfelder zum field-array hinzu
     offsets = [[-1, 0], [1, 0], [0, -1], [0, 1]]
     searched[posy][posx] = True
     for offset in offsets:
@@ -173,7 +174,7 @@ def add_num(local_field, posx, posy, searched):
     return local_field, searched
 
 
-def right_click(x, y):
+def right_click(x, y):  # rechtsklick auf feld
     global clicks, field
     if field[y][x] == -1:
         time.sleep(random.random())
@@ -183,7 +184,7 @@ def right_click(x, y):
         actions.context_click(c).perform()
 
 
-def click(x, y):
+def click(x, y):  # linksklick auf feld und updaten des feld-arrays
     global losses, total, wins, field, cells, lastchance, clicks, chances
     if field[y][x] == -1:
         clicks += 1
@@ -221,7 +222,7 @@ def click(x, y):
                 field, s = add_num(field, x, y, s)
 
 
-def both_click(posx, posy):
+def both_click(posx, posy):  # klick auf nummer, um alle felder zu clearen (shortcut der website)
     global field, clicks
     if 0 < field[posy][posx] < 9:
         time.sleep(random.random())
@@ -250,7 +251,7 @@ def both_click(posx, posy):
                             field, s = add_num(field, x, y, s)
 
 
-def restart():
+def restart():  # spiel-reset
     global field, cells, lastchance, chances, clicks
     lastchance = 0
     chances = 0
@@ -272,17 +273,17 @@ def restart():
             break
 
 
-def lost():
+def lost():  # check ob spiel verloren
     face = driver.find_element_by_xpath('//*[@id="top_area_face"]')
     return face.get_attribute('class') == loose
 
 
-def won():
+def won():  # check ob spiel gewonnen
     face = driver.find_element_by_xpath('//*[@id="top_area_face"]')
     return face.get_attribute('class') == wonwon
 
 
-def satisfied(posx, posy):
+def satisfied(posx, posy):  # check ob feld genug flaggen hat
     mines = 0
     for y in range(posy - 1, posy + 2):
         for x in range(posx - 1, posx + 2):
@@ -292,7 +293,7 @@ def satisfied(posx, posy):
     return mines == field[posy][posx]
 
 
-def round_click(posx, posy):
+def round_click(posx, posy):  # klick auf alle umliegenden clickbaren felder
     global field
     if noflag:
         clicked = False
@@ -308,7 +309,7 @@ def round_click(posx, posy):
         return True
 
 
-def place_flags(posx, posy):
+def place_flags(posx, posy):  # flaggen auf umliegende felder platzieren
     clicked = False
     clear = 0
     for y in range(posy - 1, posy + 2):
@@ -321,14 +322,14 @@ def place_flags(posx, posy):
             for x in range(posx - 1, posx + 2):
                 if 0 <= y < height and 0 <= x < width:
                     if field[y][x] == -1:
-                        if not noflag or random.random() > .5:
+                        if not noflag or random.random() > .5:  # anti-ban fügt auch im no-flag modus manchmal flaggen ein
                             right_click(x, y)
                         field[y][x] = 9
                         clicked = True
     return clicked
 
 
-def validate():
+def validate():  # gibt allen feldern eine minen-wahrscheinlickeit
     vals = [[[] for x in range(width)] for y in range(height)]
     for y in range(height):
         for x in range(width):
@@ -364,6 +365,7 @@ def validate():
     return odds
 
 
+''' -----------------ende der funktionsdeklaration-------------------'''
 found = False
 while not found:
     found = True
@@ -371,8 +373,8 @@ while not found:
         cells = find_cells()
     except:
         found = False
-field = [[-1 for x in range(width)] for y in range(height)]
-start = [[0, 0], [width - 1, 0], [width - 1, height - 1], [0, height - 1]]
+field = [[-1 for x in range(width)] for y in range(height)]  # karte des spielfeldes
+start = [[0, 0], [width - 1, 0], [width - 1, height - 1], [0, height - 1]]  # mögliche startpositionen
 while True:
     for point in start:
         time.sleep(random.random())
@@ -385,24 +387,25 @@ while True:
     else:
         break
 
-while True:
+while True:  # main loop
     # field = to_num(cells)
     placed = False
-    # place flags
+    # platziere flaggen
     for y in range(height):
         for x in range(width):
             if 0 < field[y][x] < 9:
                 if place_flags(x, y):
                     placed = True
 
-    # click empty
+    # clicke auf felder, die garantiert keine minen sind
     for y in range(height):
         for x in range(width):
             if 0 < field[y][x] < 9:
                 if satisfied(x, y):
                     round_click(x, y)
                     placed = True
-    # update ignored tiles
+    # ignoriere alle felder, um die kein clickbares feld mehr ist
+    # (macht rechnungen schneller und verhindert nutzlose clicks)
     for y in range(height):
         for x in range(width):
             if -1 < field[y][x] < 9:
@@ -415,7 +418,8 @@ while True:
                 if ignore:
                     field[y][x] = -field[y][x] - 10
 
-    # click best hope
+    # wenn nichts platziert werden konnte, rechne wahrscheinlichkeiten aus, und clicke auf das feld mit der
+    # niedrigsten wahrscheinlichkeit
     if not placed:
         if won():
             wins += 1
@@ -459,4 +463,3 @@ while True:
                         break
                 if found:
                     break
-
